9fcfdc046efb4dfeccafd5b7118f3dd5cd2c0794
DeyanTotin <138721028+DeyanTotin@users.noreply.github.com>
2024-05-06 15:01:15 +0300

    Removed POST from search movie routeer

--- run.py
+++ run.py
@@ -11,13 +11,13 @@ def home_page():
     return "<p>This is the home page<p/>"
 
 
-@app.route("/search/<string:movie_title>", methods = ["GET", "POST"])
+@app.route("/search/<string:movie_title>", methods = ["GET"])
 def search_movie(movie_title: str):
     if request.method == "GET":
         data = get_movie_by_title(movie_title)
         
         return f"<p>{data}</p>"
-        
+    
     
     
     
