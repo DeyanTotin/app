521859b4444cd49537a68e6de23381049df6bd73
DeyanTotin <138721028+DeyanTotin@users.noreply.github.com>
2024-05-07 01:34:29 +0300

    Changed get_movie_by_title() to get_movies and returns all movies movies with a similiar title

--- app/movies_services.py
+++ app/movies_services.py
@@ -1,5 +1,5 @@
 
-import json
+
 import requests
 from passwords import API_KEY
 
@@ -8,17 +8,31 @@ URL = "http://www.omdbapi.com/"
 
 
 
-def get_movie_by_title(movie_title: str):
+def get_movies(movie_title: str):
     
     params = {
         "apikey": API_KEY,
-        "t": movie_title
+        "s": movie_title,
+        "plot": "full",
+        
         
     }
     
     response = requests.get(URL, params=params)
-    json_data = json.loads(response.text)
+   
+    json_response = response.json() 
+    
+  
+    movies = []
+    
+    if "Error" in json_response:
+        return []
+    if "Search" in json_response:    
+        movies = json_response["Search"]
 
-    return json_data
-
-
+        return movies
+        
+        
+  
+        
+   
