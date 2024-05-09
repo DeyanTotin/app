f3813c29b123f48ca4c49893f11c504e779c657e
DeyanTotin <138721028+DeyanTotin@users.noreply.github.com>
2024-05-09 13:14:17 +0300

    fixed the import of the API_TOKEN

--- app/collections_services.py
+++ app/collections_services.py
@@ -1,9 +1,8 @@
 
 
 import requests
-# from passwords import API_TOKEN
+from passwords import API_TOKEN
 
-API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2M4MGFmMmY4NDI3ZmU4OTAwMWIyNzE5ZGVhNTU4YSIsInN1YiI6IjY2Mzc1ZTY1MzU4ZGE3MDEyYTU2NjA2MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kGdh3QivdUl2CUNBIRjTYExwhjz1MNlW5gFeMPN2gyA"
 
 BASE_URL = "https://api.themoviedb.org/3/"
 
