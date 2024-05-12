13ecb950983753f265b9d3429b2f0a69731971f9
DeyanTotin <138721028+DeyanTotin@users.noreply.github.com>
2024-05-12 03:48:43 +0300

    Added /movies endpoint and /collection endpoint

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, request, render_template
-from app.collections_services import get_collections
+from app.collections_services import get_collections, get_collection_by_id
 
 app = Flask(__name__)
 
@@ -11,6 +11,14 @@ def home_page():
     
     return render_template("index.html")
 
+@app.route("/movies", methods = ["GET", "POST"])
+def search_movies():
+    return render_template("movies.html")
+
+
+@app.route("/tvs", methods = ["GET", "POST"])
+def search_tvs():
+    return render_template("tvs.html")
 
 @app.route("/collections", methods = ["GET"])
 def search_collections():
@@ -18,23 +26,31 @@ def search_collections():
         
         collection_title = request.args.get("content", "").strip()
         
-        
-
         if collection_title:
    
             collections = get_collections(collection_title)
+
             
              
             
         else:
             collections = []
             
-            
+        
             
         chunks = [collections[i:i + 4] for i in range(0, len(collections), 4)]
         
         return render_template("collections.html", chunks=chunks)
 
+    
+@app.route("/collection/<id>", methods=["GET"])
+def get_collection(id):
+    
+    
+    collection = get_collection_by_id(id)
+    
+    print(collection)
+    return render_template("collection.html", collection=collection)
     
     
     
